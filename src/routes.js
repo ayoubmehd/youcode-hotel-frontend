f16006501e183e48042c17c603066c458b57bc86
ayoubmehd <safi-_-gio-raja@live.fr>
2022-01-21 12:36:23 +0100

    feat: add reservation page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
+
+// Import Your view
 import Home from "./Views/Home";
 import About from "./Views/About";
+import Reservations from "./Views/Reservations";
+
+const NotFound = () => (<h1>404 Not Found</h1>);
 
 export default [
     {
@@ -9,5 +14,21 @@ export default [
     {
         path: "/about",
         element: <About />
-    }
+    },
+    {
+        path: "/reservations",
+        element: <Reservations />
+    },
+    /**
+     * add your route like
+     * 
+     * {
+     *     path: "/your-path",
+     *     element: <Your View />
+     * },
+     */
+    {
+        path: "*",
+        element: <NotFound />
+    },
 ];
