c82390c7b307a16609c1f453a48b607dcafef97e
ayoubmehd <safi-_-gio-raja@live.fr>
2022-01-21 12:34:07 +0100

    feat: build sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,25 +7,20 @@ import routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <nav>
-          <Link to="/">Home</Link>
-          {' '}
-          <Link to="/about">About</Link>
-        </nav>
+
       </header>
-      <div>
-        <Routes>
-          {
-            routes
-              .map(({ path, element }) => <Route path={path} element={element} key={path} />)
-          }
-        </Routes>
+      <div className="flex">
+        <Sidebar />
+        <main className="flex w-full">
+          <Routes>
+            {
+              routes
+                .map(({ path, element }) => <Route path={path} element={element} key={path} />)
+            }
+          </Routes>
+        </main>
       </div>
     </div>
   );
