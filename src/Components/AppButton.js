b1d362368c98c814fe6eaa84415646a16ebb7c81
ayoubmehd <safi-_-gio-raja@live.fr>
2022-01-27 11:13:17 +0100

    feat: element type react-router link/html button

--- src/Components/AppButton.js
+++ src/Components/AppButton.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
+
 function AppButton(props) {
+
+    const Tag = props.type === "link" ? Link : "button";
+
+    const propsCopy = { ...props };
+
+    delete propsCopy.type;
+
     return (
-        <button className="min-w-36 px-4 py-2 bg-purple-600 text-white rounded shadow">
+        <Tag {...propsCopy} className="min-w-36 px-4 py-2 bg-purple-600 text-white rounded shadow">
             {props.children}
-        </button>
+        </Tag>
     );
 }
 
